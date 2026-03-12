d2d967c7511a598a03c83d32060606c91bbfa614
Ahdexito <ags120120@gmail.com>
2026-03-12 16:32:04 +0100

    solucion bugs visuales movil

--- admin/gestion_incidencias.php
+++ admin/gestion_incidencias.php
@@ -79,6 +79,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eComunitree | Gestión Incidencias</title>
     <link rel="stylesheet" href="../css/gestion/gestion.css">
     <link rel="icon" href="../img/logo-favicon.png" type="image/png">
--- cambiar_foto.php
+++ cambiar_foto.php
@@ -68,6 +68,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cambiar Foto | eComunitree</title>
     <link rel="stylesheet" href="./css/insert_edit/insert_edit.css">
     <link rel="icon" href="./img/logo-favicon.png" type="image/png">
